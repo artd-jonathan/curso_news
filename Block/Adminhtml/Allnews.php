0cd0daf81b1d0e5206a0e706f9be43360442def7
Jonathan Urzola Maldonado <jonathan.urzola@outlook.com>
2023-09-15 08:56:30 -0500

    Frontend was added

---
+++ Block/Adminhtml/Allnews.php
@@ -0,0 +1,25 @@
+<?php
+namespace Curso\News\Block\Adminhtml;
+
+class Allnews extends \Magento\Backend\Block\Widget\Grid\Container
+{
+    protected function _construct()
+    {
+        $this->_controller = 'adminhtml_allnews';
+        $this->_blockGroup = 'Curso_News';
+        $this->_headerText = __('Manage News');
+
+        parent::_construct();
+
+        if ($this->_isAllowedAction('Curso_News::save')) {
+            $this->buttonList->update('add', 'label', __('Add News'));
+        } else {
+            $this->buttonList->remove('add');
+        }
+    }
+
+    protected function _isAllowedAction($resourceId)
+    {
+        return $this->_authorization->isAllowed($resourceId);
+    }
+}
---
+++ Model/Allnews/Source/Status.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Curso\News\Model\Allnews\Source;
+
+use Magento\Framework\Data\OptionSourceInterface;
+
+class Status implements OptionSourceInterface
+{
+    protected $allNews;
+
+    public function __construct(\Curso\News\Model\Allnews $allNews)
+    {
+        $this->allNews = $allNews;
+    }
+
+    public function toOptionArray()
+    {
+        $availableOptions = $this->allNews->getAvailableStatuses();
+        $options = [];
+        foreach ($availableOptions as $key => $value) {
+            $options[] = [
+                'label' => $value,
+                'value' => $key,
+            ];
+        }
+        return $options;
+    }
+}
